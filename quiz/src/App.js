e39fe30175c369f2a41bac2c5c4f7895e0a37514
Ayush Kumar <115335623+Ayush-28-08@users.noreply.github.com>
2022-12-27 11:54:07 +0530

    quiz 2 done

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -1,12 +1,12 @@
 // import Home from './components/HomeComponent';
-// import Quiz from './components/QuizComponent';
-import Result from './components/ResultComponent';
+import Quiz from './components/QuizComponent';
+// import Result from './components/ResultComponent';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Result/>
+      <Quiz/>
   
     </div>
   );
--- quiz/src/components/QuizComponent.js
+++ quiz/src/components/QuizComponent.js
@@ -1,27 +1,50 @@
-import React  from 'react'
+import React,{Component}  from 'react'
 import './component.css'
-
-export default function Quiz() {
-
-
+import { Questions } from '../resoures/questions'
+class Quiz extends Component{
+    constructor(){
+        super()
+        this.state={
+            questionNumber:0
+        }
+    }
+    goNext(){
+        if(this.state.questionNumber<14){
+        this.setState({
+            questionNumber:this.state.questionNumber+1
+        
+        })
+    }
+    }
+    goBack(){
+        if(this.state.questionNumber>0){
+            this.setState({
+                questionNumber:this.state.questionNumber-1
+            })
+        }
+    }
+    quit(){
+        window.confirm("Are you sure you want to quit")
+    }
+    render(){
     return(
         <>
         
         <div id='quiz-div'>
             <div>
                 <h2>Questions</h2>
-                <h4>1 of 15</h4>
-                <h3>Which is the only mammal that can jump?</h3>
+                <h4>{this.state.questionNumber+1} of {Questions.length}</h4>
+                <h3>{Questions[this.state.questionNumber].question}</h3>
                 <div id='option-button-div'>
-                    <button className="option-button">Dog</button>
-                    <button className="option-button">Elephant</button>
-                    <button className="option-button">Goat</button>
-                    <button className="option-button">Lion</button>
+                    <button className="option-button">{Questions[this.state.questionNumber].optionA}</button>
+                    <button className="option-button">{Questions[this.state.questionNumber].optionB}</button>
+                    <button className="option-button">{Questions[this.state.questionNumber].optionC}</button>
+                    <button className="option-button">{Questions[this.state.questionNumber].optionD}</button>
                 </div>
                 <div id='bottom-div'>
-                    <button id='prev-button' className='bottom'>Previous</button>
-                    <button id='next-button' className='bottom'>Next</button>
-                    <button id='quit-button' className='bottom'>Quit</button>
+                    <button id='prev-button' className='bottom' onClick={()=>this.goBack()}>Previous</button>
+                    <button id='next-button' className='bottom' onClick={()=>this.goNext()}>Next</button>
+                    <button id='quit-button' className='bottom' onClick={()=>this.quit()}>Quit</button>
                 </div>
             </div>
 
@@ -29,5 +52,7 @@ export default function Quiz() {
         
         </>
     )
+    }
 
 }
+export default Quiz
